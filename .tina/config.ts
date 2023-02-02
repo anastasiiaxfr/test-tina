6aa3e5032c7594fd6bdc21833790178ff6325b96
Cornelius2017 <xforealn@gmail.com>
2023-02-02 21:45:14 +0200

    fix

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,10 +5,6 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineStaticConfig({
   clientId: process.env.TINA_CLIENT_ID!,
-  branch:
-    process.env.TINA_BRANCH! || // custom branch env override
-    process.env.VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
   build: {
     outputFolder: "admin",
